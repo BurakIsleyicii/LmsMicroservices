fa59470d0b2b1d7d34386168894a7f743c89adc9
Burak İŞLEYİCİ <burakisleyici@outlook.com>
2021-04-22 02:04:11 +0300

    Discount service added. startup configuration for identity server JWT. Controller and endpoints done.

--- IdentityServer/Lms.IdentityServer/Config.cs
+++ IdentityServer/Lms.IdentityServer/Config.cs
@@ -17,6 +17,7 @@ namespace Lms.IdentityServer
                        new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission" }},
                        new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission" }},
                        new ApiResource("resource_basket"){Scopes={"basket_fullpermission" }},
+                       new ApiResource("resource_discount"){Scopes={"discount_fullpermission" }},
                        new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
                    };
 
@@ -35,6 +36,7 @@ namespace Lms.IdentityServer
                 new ApiScope("catalog_fullpermission", "Catalog API full erisim"),
                 new ApiScope("photo_stock_fullpermission", "PhotoStock API full erisim"),
                 new ApiScope("basket_fullpermission", "PhotoStock API full erisim"),
+                new ApiScope("discount_fullpermission", "Discount API full erisim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -56,7 +58,7 @@ namespace Lms.IdentityServer
                     AllowOfflineAccess=true,
                     ClientSecrets= {new Secret("secret".Sha256())},
                     AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes={ "basket_fullpermission", "order_fullpermission", "gateway_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName,"roles" },
+                    AllowedScopes={ "basket_fullpermission", "discount_fullpermission", "order_fullpermission", "gateway_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName,"roles" },
                     AccessTokenLifetime=1*60*60,
                     RefreshTokenExpiration=TokenExpiration.Absolute,
                     AbsoluteRefreshTokenLifetime= (int) (DateTime.Now.AddDays(60)- DateTime.Now).TotalSeconds,
