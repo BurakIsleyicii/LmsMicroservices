f31e70c94a6b1661830060b4c90475e9598b90f0
Burak İŞLEYİCİ <burakisleyici@outlook.com>
2021-04-27 01:01:09 +0300

    Entity, AggregateRoot, ValueObjects added. Domain created. references edited. layers created. dbcontext create and configurations done. Dtos created and mappings done. Query and commands create with mediatR. startup configurations done. migrations done.

---
+++ Services/Order/Lms.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Lms.Services.Order.Application.Dtos;
+using Lms.Shared.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Services.Order.Application.Queries
+{
+    public class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
